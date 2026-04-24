7518c30254b4d831a504512e4181a87eb63bc1c1
Diego Ortiz <73130246+museortizmedia@users.noreply.github.com>
2026-04-24 03:30:40 -0500

    Firma XML y p12

--- server.js
+++ server.js
@@ -1,13 +1,79 @@
 import express from "express"
 import soap from "soap"
 import cors from "cors"
+import forge from "node-forge"
+import { SignedXml } from "xml-crypto"
+import { DOMParser } from "xmldom"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json({ limit: "20mb" }))
 
+function extractCert(p12Base64, password) {
+
+    const p12Der = forge.util.decode64(p12Base64)
+
+    const p12Asn1 =
+        forge.asn1.fromDer(p12Der)
+
+    const p12 =
+        forge.pkcs12.pkcs12FromAsn1(
+            p12Asn1,
+            password
+        )
+
+    let key
+    let cert
+
+    for (const safeContent of p12.safeContents) {
+
+        for (const safeBag of safeContent.safeBags) {
+
+            if (safeBag.type === forge.pki.oids.pkcs8ShroudedKeyBag) {
+                key = forge.pki.privateKeyToPem(
+                    safeBag.key
+                )
+            }
+
+            if (safeBag.type === forge.pki.oids.certBag) {
+                cert = forge.pki.certificateToPem(
+                    safeBag.cert
+                )
+            }
+        }
+    }
+
+    return { key, cert }
+}
+
+function signXml(xml, key, cert) {
+
+    const sig = new SignedXml()
+
+    sig.addReference(
+        "//*[local-name(.)='Invoice']"
+    )
+
+    sig.signingKey = key
+
+    sig.keyInfoProvider = {
+        getKeyInfo() {
+            return `<X509Data><X509Certificate>${cert
+                    .replace("-----BEGIN CERTIFICATE-----", "")
+                    .replace("-----END CERTIFICATE-----", "")
+                    .replace(/\n/g, "")
+                }</X509Certificate></X509Data>`
+        }
+    }
+
+    sig.computeSignature(xml)
+
+    return sig.getSignedXml()
+}
+
 app.post("/dian/send", async (req, res) => {
+
     try {
 
         const {
@@ -20,18 +86,26 @@ app.post("/dian/send", async (req, res) => {
             certPassword
         } = req.body
 
+        const { key, cert: certPem } =
+            extractCert(cert, certPassword)
+
+        const xmlFirmado =
+            signXml(xml, key, certPem)
+
         const wsdl =
             environment === "habilitacion"
                 ? "https://vpfe-hab.dian.gov.co/WcfDianCustomerServices.svc?wsdl"
                 : "https://vpfe.dian.gov.co/WcfDianCustomerServices.svc?wsdl"
 
-        const client = await soap.createClientAsync(wsdl)
+        const client =
+            await soap.createClientAsync(wsdl)
 
-        const security = new soap.WSSecurityCert(
-            cert,
-            certPassword,
-            { hasTimeStamp: true }
-        )
+        const security =
+            new soap.WSSecurityCert(
+                certPem,
+                key,
+                { hasTimeStamp: true }
+            )
 
         client.setSecurity(security)
 
@@ -42,7 +116,10 @@ app.post("/dian/send", async (req, res) => {
             const [response] =
                 await client.SendTestSetAsync({
                     fileName,
-                    contentFile: xml,
+                    contentFile:
+                        Buffer
+                            .from(xmlFirmado)
+                            .toString("base64"),
                     testSetId
                 })
 
@@ -53,7 +130,10 @@ app.post("/dian/send", async (req, res) => {
             const [response] =
                 await client.SendBillSyncAsync({
                     fileName,
-                    contentFile: xml
+                    contentFile:
+                        Buffer
+                            .from(xmlFirmado)
+                            .toString("base64")
                 })
 
             result = response
@@ -71,6 +151,4 @@ app.post("/dian/send", async (req, res) => {
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log("DIAN service running on port", PORT)
-})
+app.listen(PORT)
